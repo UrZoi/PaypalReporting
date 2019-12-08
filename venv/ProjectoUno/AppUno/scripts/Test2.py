428bb7bf765816d9cbaf2bfb1fbc8133d466e985
UrZoi <urzoizero@gmail.com>
2019-12-07 18:25:00 -0600

    Completed second step of retriving info

--- venv/ProjectoUno/AppUno/scripts/Test2.py
+++ venv/ProjectoUno/AppUno/scripts/Test2.py
@@ -1,5 +1,3 @@
-
-
 def monthly_difference():
     print("""In this function is really not necessary this point so i just will comment this shit and 
     let this shit begin
@@ -28,6 +26,19 @@ def get_dates():
     dates = []
     return dates
 
+def rreplace(s, old, new):
+    return (s[::-1].replace(old[::-1],new[::-1], 1))[::-1]
+
+
+def LinkGenerator(example,sheetnumber):
+    l=[]
+    for i in range(int(sheetnumber)):
+        if i != 0:
+            l.append(rreplace(str(example),str(sheetnumber),str(i+1)))
+    #print (l)
+    return l   
+
+
 
 def monthly_data(start_date, end_date):
     import requests
@@ -49,9 +60,9 @@ def monthly_data(start_date, end_date):
     response = requests.post('https://api.paypal.com/v1/oauth2/token',
                              headers=headers,
                              data=data,
-                             auth=('Here should be your auth from Paypal :) '))
+                             auth=('Heres go your Paypal key'))
     data = json.loads(response.text)
-    print(data)
+    #print(data)
 
     headers2 = {
         'Content-Type': 'application/json',
@@ -65,30 +76,58 @@ def monthly_data(start_date, end_date):
     )
     Datas = []
     response2 = requests.get('https://api.paypal.com/v1/reporting/transactions', headers=headers2, params=params)
-    print(response2.status_code)
-    print(response2.text)
+    #print(response2.status_code)
+    #print(response2.text)
 
     Datas.append(json.loads(response2.text))
 
-
-    if int(Datas[0]['total_items']) > 100:
-        print(Datas[0]['links'][0].keys())
-        print (len(Datas[0]['links']))
-        for i in range(len(Datas[0]['links'] )- 2 ):
-            #print(data2['links'][i]['href'])
-            response2 =  requests.get(Datas[0]['links'][i]['href'], headers=headers2)
-            #print(response2.text)
+    f = open("json.txt", "w")
+    f.write(str(response2.text))
+    f.close()
+    if int(Datas[0]['total_items']) > 5:
+        #print(Datas[0]['links'][0]['href'])
+        #print(Datas[0]['total_pages'])
+        lacosa = LinkGenerator(Datas[0]['links'][0]['href'],Datas[0]['total_pages'])
+        for i in range(len(lacosa)):
+            #print(Datas['links'][i]['href'])
+            #print('IAM THE RESPONSE    ' ,response2.text)
+            response2 = requests.get(lacosa[i], headers=headers2)
             Datas.append(json.loads(response2.text))
-    print(Datas[0]['total_items'])
-    print(len(Datas))
+    #print(Datas[0]['total_items'])
+   # print(len(Datas))
     #print(len(data2['account_number']))
     #print(data2['transaction_details'][0]['transaction_info']['transaction_amount'])
     #print (data2['transaction_details'][1])
 
-    f = open("json.txt", "w")
-    f.write(str(Datas))
-    f.close()
+
     return Datas
 
 
-MData = monthly_data('2019-05-01T23:20:50.52Z', '2019-06-01T23:20:50.52Z')
+def Balancesheet(MData):
+    negativebalance = []
+    #print (len(MData))
+    for j in range(len(MData)):
+        for i in range(len(MData[j]['transaction_details'])):
+
+                try:
+                    if 'CALLE CALDERON DE LA BARCA, 488' in MData[j]['transaction_details'][i]['shipping_info']['address']['line1'] or 'AVENIDA CIRCUNVALACION AGUSTIN YANEZ, 2481' in MData[j]['transaction_details'][i]['shipping_info']['address']['line1'] :
+                        print (MData[j]['transaction_details'][i]['transaction_info']['transaction_amount']['value'], MData[j]['transaction_details'][i]['shipping_info']['address']['line1'])
+                        negativebalance.append(MData[j]['transaction_details'][i]['transaction_info']['transaction_amount']['value'])
+                except:
+                    print('this transaction dont care')
+
+    num = 0
+    for i in negativebalance:
+        num += float(i)
+        print(i)
+    print(num)
+    negativebalance.append(num)
+
+    return negativebalance
+
+
+
+
+MData = monthly_data('2019-11-16T23:20:50.52Z', '2019-12-07T23:20:50.52Z')
+Balance = Balancesheet(MData)
+
